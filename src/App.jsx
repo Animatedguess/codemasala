08948014c95273f4810bf889d4b79a9541fbc092
Animatedguess <105106737+Animatedguess@users.noreply.github.com>
2025-03-20 23:54:38 +0530

    Initialize React and Vite project with Tailwind CSS integration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-cneter h-screen bg-gray-100">
+        <h1 class="text-5xl font-bold underline text-blue-500">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
